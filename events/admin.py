f45917ff3392e4742dd6db9c2e3140cc5c7b22c6
Shiven Singhal <19singsh@hitchinboys.co.uk>
2021-02-24 20:51:41 +0000

    initial commit

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,21 @@ from .models import Venue
 from .models import MyClubUser
 from .models import Event
 
-admin.site.register(Venue)
+# admin.site.register(Venue, VenueAdmin)
 admin.site.register(MyClubUser)
-admin.site.register(Event)
+# admin.site.register(Event)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'eventDate', 'description', 'manager')
+    list_display = ('name', 'eventDate', 'venue')
+    list_filter = ('eventDate', 'venue')
+    ordering = ('eventDate',)
--- events/models.py
+++ events/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zipCode = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Phone Number', max_length=25)
-    web = models.URLField('Website Address')
-    email = models.EmailField('Email Address')
+    phone = models.CharField('Phone Number', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name
@@ -27,7 +28,7 @@ class Event(models.Model):
     eventDate = models.DateTimeField('Event Date and Time')
     # venue = models.CharField(max_length=120)
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
