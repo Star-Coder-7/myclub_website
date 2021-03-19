8acfd85cf79b9f8760958df669fe2e462891b8b5
Shiven Singhal <19singsh@hitchinboys.co.uk>
2021-03-19 16:11:11 +0000

    initial commit

--- events/views.py
+++ events/views.py
@@ -7,6 +7,15 @@ from .models import Event, Venue
 from .forms import VenueForm
 
 
+def search_venues(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request, 'events/search_venues.html', {'searched': searched, 'venues': venues})
+    else:
+        return render(request, 'events/search_venues.html', {})
+
+
 def show_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     return render(request, 'events/show_venue.html', {'venue': venue})
